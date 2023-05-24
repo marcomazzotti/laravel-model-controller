a7ee17f3282e6b0884eef07d41ff4eb1a06a3553
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-24 15:23:49 +0200

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        $movies = Movie::all();
+        return view("welcome", compact("movies"));
+    }
+}
