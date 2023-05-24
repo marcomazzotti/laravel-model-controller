2c0d7168c2e83e68c8005d380c98cdf0838897cb
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-24 15:31:28 +0200

    Add: card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,25 @@
 
 <body>
 
-    <main class="bg-light">
+    <main>
+        <div class="container">
+            <div class="row row-cols-5">
+                @foreach ($movies as $movie)
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="{{ $movie->image }}" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $movie->title }}</h4>
+                                <h6 class="card-title">{{ $movie->original_title }}</h6>
+                                <p>Nationality: {{ $movie->nationality }}</p>
+                                <p>Release Date: {{ $movie->date }}</p>
+                                <p>Vote: {{ $movie->vote }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
 
 </body>
